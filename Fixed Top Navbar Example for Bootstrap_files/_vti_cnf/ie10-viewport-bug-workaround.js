861f60320e26e211d8856554ebbe59c9525c6150
LG67 <lori.gleason@gmail.com>
2015-07-25 00:19:30 -0400

    Applying Bootstrap Menu To  Existing Pages

---
+++ Fixed Top Navbar Example for Bootstrap_files/_vti_cnf/ie10-viewport-bug-workaround.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|21 Jul 2015 20:29:38 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|Archive/AwardSubmissions.html TeamAdmin/Forms.html About/Calendar.html TeamAdmin/ApplicationProcess.html css/Fixed\\ Top\\ Navbar\\ Example\\ for\\ Bootstrap.html About/67Percent.html css/Template.dwt Archive/EngineeringDesign.html Archive/TeamHistory.html TeamAdmin/Governance.html About/CommunityOutreach.html About/ContactUs.html Resources/CAD.html
