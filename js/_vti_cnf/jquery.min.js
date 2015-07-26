60065d904771dfffc1b2d61dfbee5a53107b7fe9
LG67 <lori.gleason@gmail.com>
2015-07-25 22:09:02 -0400

    JS files for BS Menu moved to JS folder.

---
+++ js/_vti_cnf/jquery.min.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|25 Jul 2015 04:21:32 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|About/Calendar.html Archive/AwardSubmissions.html Resources/machining.html TeamAdmin/Forms.html About/67Percent.html TeamAdmin/ApplicationProcess.html css/Template.dwt About/CommunityOutreach.html Archive/EngineeringDesign.html Archive/TeamHistory.html TeamAdmin/Governance.html About/ContactUs.html Resources/CAD.html
