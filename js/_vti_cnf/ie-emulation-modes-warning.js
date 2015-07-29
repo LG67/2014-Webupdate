7c7a22e339ec1ff8bc75dccafe04d1f91f9a8a64
LG67 <lori.gleason@gmail.com>
2015-07-29 01:43:59 -0400

    Merge JS and Image Folders

--- js/_vti_cnf/ie-emulation-modes-warning.js
+++ js/_vti_cnf/ie-emulation-modes-warning.js
@@ -1,9 +1,9 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|25 Jul 2015 04:21:32 -0000
+vti_timelastmodified:TR|27 Jul 2015 03:59:31 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_author:SR|LORIG_LAPTOP\\Lori
 vti_modifiedby:SR|LORIG_LAPTOP\\Lori
 vti_timecreated:TR|25 Jul 2015 04:21:32 -0000
-vti_cacheddtm:TX|25 Jul 2015 04:21:32 -0000
-vti_filesize:IR|2181
 vti_backlinkinfo:VX|
+vti_cacheddtm:TX|27 Jul 2015 03:59:31 -0000
+vti_filesize:IR|2181
