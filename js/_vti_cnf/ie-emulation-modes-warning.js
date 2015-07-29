b38743ae06381ae2b4ddf3100d15c4bdeaa33e0f
Paul Bunyan <hal7df@gmail.com>
2015-07-28 23:14:09 -0400

    Testing removing vti files.

--- js/_vti_cnf/ie-emulation-modes-warning.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|25 Jul 2015 04:21:32 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_author:SR|LORIG_LAPTOP\\Lori
-vti_modifiedby:SR|LORIG_LAPTOP\\Lori
-vti_timecreated:TR|25 Jul 2015 04:21:32 -0000
-vti_cacheddtm:TX|25 Jul 2015 04:21:32 -0000
-vti_filesize:IR|2181
-vti_backlinkinfo:VX|
--- js/_vti_cnf/ie10-viewport-bug-workaround.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|25 Jul 2015 04:21:32 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|About/Calendar.html Archive/AwardSubmissions.html Resources/machining.html TeamAdmin/Forms.html About/67Percent.html TeamAdmin/ApplicationProcess.html css/Template.dwt TeamAdmin/Governance.html Archive/EngineeringDesign.html Archive/TeamHistory.html About/CommunityOutreach.html About/ContactUs.html Resources/CAD.html index.html
-vti_author:SR|LORIG_LAPTOP\\Lori
-vti_modifiedby:SR|LORIG_LAPTOP\\Lori
-vti_timecreated:TR|25 Jul 2015 04:21:32 -0000
-vti_cacheddtm:TX|25 Jul 2015 04:21:32 -0000
-vti_filesize:IR|664
